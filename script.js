e725fb67cd3ec57b30ce3422aa8f54d0b4e02372
Luke Arenas <lukedarenas@gmail.com>
2021-02-08 10:23:59 -0700

    set up the moveRight function

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 console.log('hello')
 //GLOBAL VARIABLES
 const playButton = document.querySelector('#play-button')
+const stopButton = document.querySelector('#stop-button')
 const main = document.querySelector('main')
+let gameDirection = 'right'
+let currentCellID = 0
 
 //FUNCTIONS
-const createBoard = () => {
-  for (let i = 0; i < 16; i++) {
-    let newCell = document.createElement('div')
-    newCell.setAttribute('class', 'board')
-    newCell.setAttribute('id', `${i}`)
-    main.appendChild(newCell)
+// const createBoard = () => {
+//   for (let i = 0; i < 16; i++) {
+//     let newCell = document.createElement('div')
+//     newCell.setAttribute('class', 'board')
+//     newCell.setAttribute('id', `cell${i}`)
+//     newCell.innerText = 'hello'
+//     main.appendChild(newCell)
+//   }
+// }
+
+const beginGame = () => {
+  const firstPosition = document.querySelector('#cell0')
+  firstPosition.setAttribute('class', 'snake')
+  for (i = 0; i < 4; i++) {
+    if (gameDirection === 'right') {
+      window.setTimeout(moveRight, 2000)
+    }
   }
+}
+
+const moveRight = () => {
+  let step = currentCellID + 1
+  const nextDiv = document.querySelector(`#cell${currentCellID}`) //get div
+  const nextCellID = nextDiv.getAttribute('id') //get id
+  const nextID = parseInt(nextCellID.replace('cell', '')) //parseint id
+  const nextCell = document.querySelector(`#cell${nextID}`) //assign next cell
+  console.log(nextCell)
+  nextCell.setAttribute('class', 'snake')
+  currentCellID = step
 }
 
 //EVENT LISTENERS
 
-// playButton.addEventListener('click', create)
+playButton.addEventListener('click', beginGame)
